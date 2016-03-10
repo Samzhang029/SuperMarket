eb7b75635fd5aec9e6153ed510e89b2f85703d2a
Sam Zhang (Beyondsoft Corporation) <v-sazh@microsoft.com>
2016-03-10 18:01:26 +0800

    PromotionDiscount is finished.

---
+++ SuperMarket/SuperMarketPrinter/DiscountPromotion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarketPrinter
+{
+    class DiscountPromotion : Promotion
+    {
+        private Smallware promotionGoods;
+        public DiscountPromotion(Smallware goods)
+        {
+            promotionGoods = goods;
+        }
+        
+        public decimal Discount { get; set; }
+
+        public override void DoPromotioin()
+        {            
+            decimal currentPrice = this.promotionGoods.UnitPrice;
+            DiscountPrice = currentPrice * Discount;
+
+            TotalAfterPromotion = DiscountPrice * promotionGoods.Count;
+            SavedMoney = promotionGoods.Total - TotalAfterPromotion;
+        }
+
+        public override string ToString()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ SuperMarket/SuperMarketPrinter/Smallware.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarketPrinter
+{
+    
+    public class Smallware
+    {
+        public string Name { get; set; }
+
+        public Category Category { get; set; }
+
+        public string UnitName { get; set; }
+
+        public int Count { get; set; }
+                
+        public decimal UnitPrice { get; set; }
+
+        public decimal Total {
+            get {
+                return UnitPrice * Count;       
+            }
+        }
+
+        public string BarCode { get; set; }
+
+    }
+
+    public enum Category { Food, Goods }
+}
