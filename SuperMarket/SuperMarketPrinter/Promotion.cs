b72a2d06c03f9a48a3c1aca97d7210abee6b3581
Sam Zhang (Beyondsoft Corporation) <v-sazh@microsoft.com>
2016-03-10 18:03:22 +0800

    Finished Goods and DiscountPromotion.

---
+++ SuperMarket/SuperMarketPrinter/Promotion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarketPrinter
+{
+    public abstract class Promotion
+    {
+        public Smallware Goods{get; set;}
+
+        public decimal DiscountPrice { get; set; }
+
+        public decimal TotalAfterPromotion { get; set; }
+        
+        public decimal SavedMoney { get; set; }
+
+        public int GiftCount { get; set; }
+
+        public abstract void DoPromotioin();
+
+        public abstract string ToString();
+    }
+}
---
+++ SuperMarket/SuperMarketPrinter/Promotion3For1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarketPrinter
+{
+    class Promotion3For1 : Promotion
+    {
+        private Smallware promotionGoods;
+        public Promotion3For1(Smallware goods)
+        {
+            promotionGoods = goods;
+        }
+        
+        public override void DoPromotioin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
